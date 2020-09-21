5967eca4fee0840bc9d79bb29d2c0bce0f219818
Евгений <mitushinem@gmail.com>
2020-09-21 17:16:27 +0300

    Update setting_centos.sh

--- setting_centos.sh
+++ setting_centos.sh
@@ -29,4 +29,18 @@ systemctl disabled firewalld
 sudo sed -i '$a alias netstat="netstat -tulpn"' ./.bashrc
 source ./.bashrc
 
+echo 'Установка Zabbix proxy 5.0.1'
+dnf -y install http://mirror.centos.org/centos/8.0.1905/AppStream/x86_64/os/Packages/libssh2-1.8.0-8.module_el8.0.0+189+f9babebb.1.x86_64.rpm
+rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/8/x86_64/zabbix-release-5.0-1.el8.noarch.rpm
+dnf clean all
+dnf -y install zabbix-proxy-mysql
+
+dnf -y install mariadb-server && systemctl start mariadb && systemctl enable mariadb
+mysql_secure_installation
+mysql -uroot -p'qwer1234++' -e "create database zabbix_proxy character set utf8 collate utf8_bin;"
+mysql -uroot -p'qwer1234++' -e "grant all privileges on zabbix_proxy.* to zabbix@localhost identified by 'zabbixDBpass';"
+mysql -uroot -p'qwer1234++' zabbix_proxy -e "set global innodb_strict_mode='OFF';"
+zcat /usr/share/doc/zabbix-proxy-mysql*/schema.sql.gz |  mysql -uzabbix -p'qwer1234++' zabbix_proxy
+mysql -uroot -p'rootDBpass' zabbix_proxy -e "set global innodb_strict_mode='ON';"
+
 reboot
